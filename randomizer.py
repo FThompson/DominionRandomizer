a84972b5b3b5fc0cdb59f5f9c86508e994fa263e
Finn Thompson <nnifinn@gmail.com>
2018-12-25 13:00:05 -0800

    Added documentation.

--- randomizer.py
+++ randomizer.py
@@ -9,6 +9,31 @@ from dtypes import BasicCard, Card, CardType, GameSet, SpecialTypeCard, SplitPil
 class Randomizer():
     def __init__(self, data_path, sets, number=10, weights=None, counts=None, include=None, exclude=None,
                  filter_types=None, n_events=0, n_landmarks=0):
+        """
+        Creates a randomizer with given arguments.
+        
+        :param data_path: The cards.json file path
+        :type data_path: str
+        :param sets: The names of the sets to randomize, optionally in argument form (i.e. base2e)
+        :type sets: List[str]
+        :param number: The total number of cards to pick, defaults to 10
+        :type number: int, optional
+        :param weights: The weights to be applied to each set when randomly picking cards, defaults to None
+        :type weights: List[int], optional
+        :param counts: The counts of cards to pick from each set, defaults to None
+        :type counts: List[int], optional
+        :param include: The cards to include, optionally in argument form, defaults to None
+        :type include: List[str], optional
+        :param exclude: The cards to exclude, optionally in argument form, defaults to None
+        :type exclude: List[str], optional
+        :param filter_types: The card types to filter out before randomly picking cards, defaults to None
+        :type filter_types: List[str], optional
+        :param n_events: The number of events to pick, defaults to 0
+        :type n_events: int, optional
+        :param n_landmarks: The number of landmarks to pick, defaults to 0
+        :type n_landmarks: int, optional
+        """
+
         self.data_path = data_path
         self.sets = GameSet.complete_sets() if 'all' in sets else [GameSet.for_arg(set_arg) for set_arg in sets]
         self.number = number
@@ -33,7 +58,11 @@ class Randomizer():
         self.add_exclusions()
 
     def print_cards(self):
-        # convert game sets to set names to combine editioned sets under one key
+        """
+        Prints the randomizeds cards, listed by set name.
+        
+        """
+
         cards = defaultdict(list)
         for game_set, set_cards in self.cards.items():
             cards[game_set.set_name].extend(set_cards)
@@ -46,6 +75,17 @@ class Randomizer():
         self.print_non_cards(self.landmarks, 'Landmarks', False)
 
     def print_non_cards(self, card_list, label, print_cost):
+        """
+        Prints the given list of non-card cards (i.e. Events, Landmarks) with the given label.
+        
+        :param card_list: The cards to print.
+        :type card_list: List[Card]
+        :param label: The header to print the cards under.
+        :type label: str
+        :param print_cost: True to print the card cost, otherwise False
+        :type print_cost: bool
+        """
+
         if len(card_list) > 0:
             print(label)
             for card in card_list:
@@ -55,6 +95,12 @@ class Randomizer():
                     print('- %s, %s' % (card.name, card.game_set))
 
     def randomize(self):
+        """
+        Picks random cards based on the randomizer's parameters.
+        
+        """
+
+
         if self.mode == 'counted':
             counts = {self.sets[i]: self.counts[i] for i in range(len(self.sets))}
         else:
@@ -75,9 +121,25 @@ class Randomizer():
         self.landmarks = random.sample(self.possible_landmarks, self.n_landmarks)
 
     def randomize_set(self, game_set, count):
+        """
+        Picks the given count of cards from the given game set.
+        
+        :param game_set: The game set to pick cards from in the possible card list.
+        :type game_set: GameSet
+        :param count: The count of cards to pick.
+        :type count: int
+        :return: The list of random cards picked from the set.
+        :rtype: List[Card]
+        """ 
+
         return random.sample(self.possible_cards[game_set], k=count)
 
     def load_cards(self):
+        """
+        Loads cards from the data path and populates possible card lists.
+        
+        """
+
         with open(self.data_path) as f:
             data = json.load(f)
             self.all_cards = [Card.from_json(**d) for d in data]
@@ -91,20 +153,51 @@ class Randomizer():
         self.possible_landmarks = self.get_non_cards('Landmark', self.n_landmarks)
 
     def get_non_cards(self, category, count):
+        """
+        Gets all possible cards of the given category, throwing an error if fewer than the given number exist.
+        
+        :param category: The card category to get.
+        :type category: str
+        :param count: The desired count of cards. Used to throw an error if not possible to get this amount.
+        :type count: int
+        :raises argparse.ArgumentTypeError: Throws an argparse error if fewer than the requested number of cards exist.
+        :return: The list of possible cards of the given category.
+        :rtype: List[Card]
+        """
+
         card_list = [c for c in self.all_cards if c.category == category and self.any_set_contains(c)]
         if count > len(card_list):
             raise argparse.ArgumentTypeError('too few %ss available in given sets: requested %d' % (category, count))
         return card_list
 
     def any_set_contains(self, card):
+        """
+        Checks if any set in the randomizer contains the given card.
+        
+        :param card: The card to check for.
+        :type card: Card
+        :return: True if the card is contained by this randomizer's sets, otherwise False.
+        :rtype: bool
+        """
+
         return any(game_set.contains(card) for game_set in self.sets)
 
     def add_special_type_cards(self):
+        """
+        Adds special randomizer cards into the list of possible cards to pick.
+        
+        """
+
         for card in SpecialTypeCard:
             if card.value.game_set in self.sets:
                 self.possible_cards[card.value.game_set].append(card.value)
 
     def remove_split_pile_cards(self):
+        """
+        Replaces the split pile cards with their corresponding split randomizer cards.
+        
+        """
+
         for split_card in SplitPileCard:
             if split_card.value.game_set in self.sets:
                 card_splits = split_card.value.name.split('/')
@@ -112,6 +205,11 @@ class Randomizer():
                 set_cards[:] = [card for card in set_cards if card.name not in card_splits]
 
     def add_inclusions(self):
+        """
+        Adds the included cards and removes them from the possible card pool and adjusts pick counts if necessary.
+        
+        """
+
         for card in self.get_name_filtered_cards(self.include, '-i/--include'):
             self.included_cards.append(card)
             self.remove_card_from_pool(card)
@@ -119,22 +217,51 @@ class Randomizer():
             self.adjust_counts()
 
     def adjust_counts(self):
+        """
+        Decrements each set's pick count based on included cards' sets.
+        
+        """
+
         for card in self.included_cards:
             for i in range(len(self.sets)):
                 if self.sets[i].contains(card):
                     self.counts[i] -= 1
 
     def add_exclusions(self):
+        """
+        Removes the excluded cards from the possible card pool.
+        
+        """
+
         for card in self.get_name_filtered_cards(self.exclude, '-x/--exclude'):
             self.remove_card_from_pool(card)
 
     def remove_card_from_pool(self, card):
+        """
+        Removes the given card from the possible card pool.
+        
+        :param card: The card to remove from the pool.
+        :type card: Card
+        """
+
         for game_set in self.possible_cards:
             if game_set.contains(card):
                 self.possible_cards[game_set].remove(card)
-                return  # cards are unique, may exit function once found
+                break  # cards are unique, may exit function once found
 
     def get_name_filtered_cards(self, card_args, arg_hint):
+        """
+        Parses the given card arguments or throws an argparse error with the given hint if unable to find a card.
+        
+        :param card_args: The card arguments to parse.
+        :type card_args: List[str]
+        :param arg_hint: The argument hint to include in the error if necessary.
+        :type arg_hint: str
+        :raises argparse.ArgumentTypeError: Throws an error if the requested card could not be found.
+        :return: The list of cards matching the card arguments.
+        :rtype: List[Card]
+        """
+
         cards = []
         if card_args:
             for card_arg in card_args:
@@ -148,20 +275,84 @@ class Randomizer():
         return cards
 
     def is_possible_card(self, card):
+        """
+        Checks if the given card can be picked and is not type filtered.
+        
+        :param card: The card to check.
+        :type card: Card
+        :return: True if the card is a possible card for this randomizer, otherwise False.
+        :rtype: bool
+        """
+
         return card.can_pick and not Randomizer.in_type_filter(card, self.filter_types)
 
     @staticmethod
     def in_type_filter(card, types):
+        """
+        Checks if the given card has any of the given types.
+        
+        :param card: The card to check.
+        :type card: Card
+        :param types: The list of types to check for.
+        :type types: List[str]
+        :return: True if the card has any of the given types.
+        :rtype: bool
+        """
+
         return any(t.lower() in types for t in card.types) if types else False
 
 
 class RandomizerParser():
+    """
+    An argument parser that parses the following arguments and converts them into a Randomizer instance.
+
+    positional arguments:
+        {base1e,base2e,intrigue1e,intrigue2e,seaside,alchemy,prosperity,cornucopia,hinterlands,darkages,guilds,
+            adventures,empires,nocturne,renaissance,all}
+                                Game sets to choose from, or all
+
+    optional arguments:
+        -h, --help            show this help message and exit
+        -n NUMBER, --number NUMBER
+                                Number of cards to pick, default 10
+        -w WEIGHTS [WEIGHTS ...], --weights WEIGHTS [WEIGHTS ...]
+                                Weights to be applied to each set when randomly
+                                picking cards
+        -c COUNTS [COUNTS ...], --counts COUNTS [COUNTS ...]
+                                Counts of cards to pick from each set
+        -i INCLUDE [INCLUDE ...], --include INCLUDE [INCLUDE ...]
+                                Specific cards to include
+        -x EXCLUDE [EXCLUDE ...], --exclude EXCLUDE [EXCLUDE ...]
+                                Specific cards to exclude
+        -f {action,treasure,victory,attack,duration,reaction,doom,fate,gathering,looter,night,reserve,traveller}
+            [{TYPES} ...], --filter-types {TYPES} [{TYPES} ...]
+                                Specific cards types to filter out before randomly
+                                picking cards
+        -e EVENTS, --events EVENTS
+                                Number of events to pick
+        -l LANDMARKS, --landmarks LANDMARKS
+                                Number of landmarks to pick
+    """
+
     def get_randomizer(self, data_path):
+        """
+        Construct a Randomizer instance from the parsed arguments and the given data path.
+        
+        :param data_path: The cards.json data path.
+        :type data_path: str
+        :return: A randomizer instance.
+        :rtype: Randomizer
+        """
+
         return Randomizer(data_path, self.args.sets, self.args.number, self.args.weights, self.args.counts,
                           self.args.include, self.args.exclude, self.args.filter_types, self.args.events,
                           self.args.landmarks)
 
     def parse_args(self):
+        """
+        Creates the argparse.ArgumentParser and parses and checks args.
+        """
+
         self.parser = argparse.ArgumentParser()
         game_choices = [g.as_arg() for g in GameSet.complete_sets()]
         game_choices.append('all')
@@ -186,17 +377,37 @@ class RandomizerParser():
         self.check_args()
 
     def check_edition_args(self, set_name):
+        """
+        Checks if the set args contain both editions of the given set name.
+        Throws a parser error if not.
+        
+        :param set_name: The set name to check for both editions.
+        :type set_name: str
+        """
+
         first = set_name + '1e'
         second = set_name + '2e'
         if first in self.args.sets and second in self.args.sets:
             self.parser.error('must choose only one of %s, %s' % (first, second))
 
     def check_distribution_args(self, dist_arg):
+        """
+        Checks if the given distribution has the same number of entries as there are sets.
+        Throws a parser error if not.
+        
+        :param dist_arg: The distribution argument to check.
+        :type dist_arg: str
+        """
+
         if dist_arg and len(self.args.sets) != len(dist_arg):
             self.parser.error('must have equal quantities of sets (%d) and weights (%d)' %
                               (len(self.args.sets), len(dist_arg)))
 
     def check_args(self):
+        """
+        Checks for conflicting editions, invalid distribution counts, and invalid inclusion counts.
+        """
+
         self.check_edition_args('base')
         self.check_edition_args('intrigue')
         self.check_distribution_args(self.args.weights)
@@ -208,13 +419,29 @@ class RandomizerParser():
 
     @staticmethod
     def standardize_input(string):
+        """
+        Standardizes argument input by lowercasing and removing apostrophes and spaces.
+        
+        :param string: The string to standardize.
+        :type string: str
+        :return: The standardized string.
+        :rtype: str
+        """
+
         return string.replace("'", '').replace(' ', '').lower()
 
 
-if __name__ == '__main__':
+def main():
+    """
+    Parses randomizer args, creates a randomizer, and randomizes and prints cards.
+    """
+
     json_path = os.path.join(os.path.dirname(__file__), 'res/cards.json')
     parser = RandomizerParser()
     parser.parse_args()
     randomizer = parser.get_randomizer(json_path)
     randomizer.randomize()
     randomizer.print_cards()
+
+if __name__ == '__main__':
+    main()
